a7604dc1cb0ee5de83664b5d3d52f4a9734e1f48
Alaux <73968015+MrAlaux@users.noreply.github.com>
2023-01-08 06:47:54 -0300

    Don't draw Berserk in fullscreen HUD
    Additionally, reformatted some of the Berserk drawing code.

--- src/st_stuff.c
+++ src/st_stuff.c
@@ -887,32 +887,29 @@ void ST_drawWidgets(void)
   }
 
   // [Nugget]: [crispy] draw berserk pack instead of no ammo if appropriate
-  if ((!st_crispyhud || nughud.ammo.x > -1) // [Nugget] Nugget HUD
+  if ((screenblocks < CRISPY_HUD || (st_crispyhud && nughud.ammo.x > -1)) // [Nugget] Nugget HUD
       && show_berserk && plyr->readyweapon == wp_fist
       && weaponinfo[plyr->readyweapon].ammo == am_noammo // [Nugget] Check for unlimited ammo type
       && plyr->powers[pw_strength])
   {
     static int lump = -1;
+    int delta;
     patch_t *patch;
 
-    if (lump < 0) {
-      lump = (W_CheckNumForName)("PSTRA0", ns_sprites); // [Nugget] ns_global does NOT work in this case
+    if (lump < 0
+        // [Nugget] Note: ns_global does NOT work in this case
+        && ((lump = (W_CheckNumForName)("PSTRA0", ns_sprites)) < 0)
+        && ((lump = (W_CheckNumForName)("MEDIA0", ns_sprites)) < 0))
+      { return; } // Couldn't find a lump
 
-      if (lump < 0)
-      { lump = (W_CheckNumForName)("MEDIA0", ns_sprites); }
-    }
+    delta = WIDESCREENDELTA * nughud.ammo.wide * st_widecrispyhud; // [Nugget]
+    patch = W_CacheLumpNum(lump, PU_CACHE);
 
-    // [Nugget] One extra check, just in case
-    if (lump > 0) {
-      int delta = (st_widecrispyhud ? WIDESCREENDELTA*nughud.ammo.wide : 0); // [Nugget]
-      patch = W_CacheLumpNum(lump, PU_CACHE);
-
-      // [crispy] (23,179) is the center of the Ammo widget
-      // [Nugget] Nugget HUD
-      V_DrawPatch((st_crispyhud ? nughud.ammo.x : ST_AMMOX) - 21 - SHORT(patch->width)/2 + SHORT(patch->leftoffset) + delta,
-                  (st_crispyhud ? nughud.ammo.y : ST_AMMOY) + 8 - SHORT(patch->height)/2 + SHORT(patch->topoffset),
-                  FG, patch);
-    }
+    // [crispy] (23,179) is the center of the Ammo widget
+    // [Nugget] Nugget HUD
+    V_DrawPatch((st_crispyhud ? nughud.ammo.x : ST_AMMOX) - 21 - SHORT(patch->width)/2 + SHORT(patch->leftoffset) + delta,
+                (st_crispyhud ? nughud.ammo.y : ST_AMMOY) + 8 - SHORT(patch->height)/2 + SHORT(patch->topoffset),
+                FG, patch);
   }
 
   if (st_crispyhud) { // [Nugget] Nugget HUD
