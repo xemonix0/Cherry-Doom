8e6c49d2a050ada53847ca04abaae6a27fb6e897
Spaicrab <giuliodf2005@gmail.com>
2024-11-02 22:48:05 +0100

    Fixed "Less Blinding Tints" bug that caused the palette to be translated multiple times

--- src/g_game.c
+++ src/g_game.c
@@ -1418,6 +1418,11 @@ static void G_DoLoadLevel(void)
     memset (joybuttons, 0, sizeof(joybuttons));
   }
 
+  // [Cherry] Less Blinding tints
+  if STRICTMODE(less_blinding_tints) {
+    I_TranslatePalette();
+  }
+
   //jff 4/26/98 wake up the status bar in case were coming out of a DM demo
   // killough 5/13/98: in case netdemo has consoleplayer other than green
   ST_Start();
@@ -4357,11 +4362,6 @@ void G_PlayerReborn(int player)
     p->maxammo[i] = maxammo[i];
 
   levels_completed = 0; // [Cherry]
-
-  // [Cherry] Less Blinding tints
-  if STRICTMODE(less_blinding_tints) {
-    I_TranslatePalette();
-  }
 }
 
 //
